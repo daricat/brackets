0d8e441ca4d7a18e19a4a0dc0f019215127a3409
daricat <darina.talk@gmail.com>
2020-09-10 13:48:08 +0300

    fix: fix some bugs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
   bracketsConfig = bracketsConfig.map((element) => element.join(""));
-  bracketsConfig.forEach((element) => {
-    while (str.includes(element)) {
-        str = str.replace(element, '');
-    }
-  });
-  str.length === 0 ? true : false;
-}  
+  let i = 0;
+  while (i < bracketsConfig.length) {
+    if (str.includes(bracketsConfig[i])) {
+      while(str.includes(bracketsConfig[i])) {
+        str = str.replace(bracketsConfig[i], '');
+      }
+      i = 0;
+    } else {
+      i += 1;
+    }    
+  }
+  return str.length === 0 ? true : false;
+}
